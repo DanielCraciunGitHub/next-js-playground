7124546ca45ef290e1bc8b390754fa0e8fc6e786
Daniel <danielcraciun111@yahoo.com>
2023-08-22 14:20:22 +0100

    nodemailer + react-email example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
-import ClientComponentExample from "@/components/ClientComponentExample"
-import RouteHandlerExample from "@/components/RouteHandlerExample"
-import ServerActionExample from "@/components/ServerActionExample"
-import ServerComponentExample from "@/components/ServerComponentExample"
-
 const page = async () => {
   return (
     <main className="flex flex-grow flex-col items-center justify-center space-y-10">
-      <ClientComponentExample />
-      <ServerComponentExample />
-      <RouteHandlerExample />
-      <ServerActionExample />
+      POST api/email with the template in emails/email.tsx
     </main>
   )
 }
--- src/env.mjs
+++ src/env.mjs
@@ -2,18 +2,8 @@ import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
 
 export const env = createEnv({
-  client: {
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(1),
-    NEXT_PUBLIC_ACCESS_TOKEN: z.string().min(1),
-    NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
-  },
   server: {
     NODEMAILER_EMAIL: z.string(),
     NODEMAILER_PASSWORD: z.string(),
-  },
-  experimental__runtimeEnv: {
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    NEXT_PUBLIC_ACCESS_TOKEN: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
   },
 })
